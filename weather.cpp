8b95ff67dac97ce6280631ad93be29a5a269996f
Danish Mujeeb <danish.mujeeb@gmail.com>
2025-07-02 22:53:49 +0000

    added new weather suggestor

---
+++ weather.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string data;
+    std::string yea;
+
+
+    std::cout << "Is there a storm or rain near you: " << std::endl;
+    std::cin >> data;
+    
+
+
+    if(data == "yes") {
+        std::cout << "Maybe skip the walk";
+        std::cout << " or take an uber" << std::endl;
+    }
+    else if(data == "no") {
+        std::cout << "What's the temp in celcius: " << std::endl;
+        std::cin >> yea;
+
+        int cyea = std::stoi(yea);
+
+        if(cyea >= 25) {
+            std::cout << " Perfect weather! Take a walk or a subway or even the bus" << std::endl;
+
+        }
+        else if(cyea < 25) {
+            std::cout << "Little chilly out there, get a light jacket or/and a sweater, walk or take the subway" << std::endl;
+
+        }
+        else if (cyea <= 10) {
+            std::cout << "Very chilly, bundle up take an uber or even the subway" << std::endl;
+
+        }
+        else if (cyea <= 0) {
+            std::cout << "Wear a down jacket if you have to but probably don\'t go out" << std::endl;
+
+        }
+
+    }
+}
+
+// No browser!!!
